99dd075a558561f273e294d67574aa1813235634
Joshua Timberman <jtimberman@cider.local>
2009-01-27 19:32:01 -0700

    volume id is needed when not replicating, too

--- lib/ec2_mysql/command.rb
+++ lib/ec2_mysql/command.rb
@@ -147,8 +147,8 @@ class Ec2Mysql
     end
     
     def slave
+      raise "You must supply -v,--volume-id to bootstrap the slave from" unless @volume_id
       if @mysql_start_replication
-        raise "You must supply -v,--volume-id to bootstrap the slave from" unless @volume_id
         raise "You must supply -h,--mysql-host to configure the master" unless @mysql_host
         raise "You must supply -U,--mysql-rep-username to configure the master" unless @mysql_rep_username
         raise "You must supply -P,--mysql-rep-password to configure the master" unless @mysql_rep_password
